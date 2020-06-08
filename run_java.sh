d825c9a4bd6eb2bec583ce5cebe376179aadae18
aligas1 <alaligas@gmail.com>
2020-06-08 19:26:08 -0400

    Update run_java.sh

--- run_java.sh
+++ run_java.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
 
+sudo run-apt update
+
+# use jd's commands
+
+# find java home by typing which java 
+
+# rewrite script and try tomorrow
+
+# used blank clone server for testing use blank1 for testing automation
+
+# also use command: sudo bash run_java.sh
+# so you don't have to try to export pw variables
+
 # Java 1.8
 
 # install java in /opt
