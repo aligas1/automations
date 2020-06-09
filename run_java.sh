3cc1d78f08b698da9b31d6785bbc75c07505030e
aligas1 <alaligas@gmail.com>
2020-06-09 15:36:27 -0400

    Update run_java.sh

--- run_java.sh
+++ run_java.sh
@@ -1,32 +1,19 @@
 #!/bin/sh
 
-sudo apt-get update
-
-# use jd's commands
-
-# find java home by typing which java 
-
-# rewrite script and try tomorrow
-
-# used blank clone server for testing use blank1 for testing automation
-
-# also use command: sudo bash run_java.sh
-# so you don't have to try to export pw variables
-
 # Java 1.8
 
 # install java in /opt
 cd ~
 cd /opt
 
-echo "1234" > pw.txt
+# answer Yes to Are you sure type questions.
+echo "y" > Yes.txt
 
-# get Java 8, unzip file and remove 
-sudo apt install openjdk-8-jdk-headless openjre-8-headless < pw.txt
-sudo tar -xzvf jdk-8u221-linux-x64.tar.gz < pw.txt
-sudo rm jdk-8u221-linux-x64.tar.gz < pw.txt
+# install openssh
+sudo apt install openssh-server openssh-client -y
 
-rm pw.txt
+# install Java 8 on Ubuntu 18.04
+sudo apt install openjdk-8-jdk-headless < Yes.txt
 
 # move to home dir, adjust .bashrc
 cd ~
@@ -36,7 +23,7 @@ source ~/.bash_profile
 
 # define path for JAVE_HOME
 echo "
-export JAVA_HOME=/opt/jdk1.8.0_221
+export JAVA_HOME=/usr/bin/java
 export PATH=\$PATH:\$JAVA_HOME/bin " >> .bash_profile
   
 source .bash_profile
